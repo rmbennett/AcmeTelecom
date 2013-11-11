313636f45b8524b28920392341482298c505bdd2
Yong Wen Chua <lawliet89@gmail.com>
2013-11-11 23:56:24 +0000

    Modified Runner to dump all the tariffs

--- src/com/acmetelecom/Runner.java
+++ src/com/acmetelecom/Runner.java
@@ -1,5 +1,7 @@
 package com.acmetelecom;
 
+import com.acmetelecom.customer.Tariff;
+
 import java.util.List;
 
 /**
@@ -12,22 +14,26 @@ import java.util.List;
 public class Runner {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Running...");
-        BillingSystem billingSystem = new BillingSystem();
-        billingSystem.callInitiated("447722113434", "447766511332");
-        sleepSeconds(20);
-        billingSystem.callCompleted("447722113434", "447766511332");
-        billingSystem.callInitiated("447722113434", "447711111111");
-        sleepSeconds(30);
-        billingSystem.callCompleted("447722113434", "447711111111");
-        billingSystem.callInitiated("447777765432", "447711111111");
-        sleepSeconds(60);
-        billingSystem.callCompleted("447777765432", "447711111111");
-        List<Bill> bills = billingSystem.createCustomerBills();
-
-        for (Bill bill : bills) {
-            bill.send();
+        Tariff tariffs[] = Tariff.values();
+        for (Tariff tariff : tariffs) {
+            System.out.println(tariff.name() + " " + tariff.offPeakRate() + " " + tariff.peakRate());
         }
+//        System.out.println("Running...");
+//        BillingSystem billingSystem = new BillingSystem();
+//        billingSystem.callInitiated("447722113434", "447766511332");
+//        sleepSeconds(20);
+//        billingSystem.callCompleted("447722113434", "447766511332");
+//        billingSystem.callInitiated("447722113434", "447711111111");
+//        sleepSeconds(30);
+//        billingSystem.callCompleted("447722113434", "447711111111");
+//        billingSystem.callInitiated("447777765432", "447711111111");
+//        sleepSeconds(60);
+//        billingSystem.callCompleted("447777765432", "447711111111");
+//        List<Bill> bills = billingSystem.createCustomerBills();
+//
+//        for (Bill bill : bills) {
+//            bill.send();
+//        }
     }
     private static void sleepSeconds(int n) throws InterruptedException {
         Thread.sleep(n * 1000);
