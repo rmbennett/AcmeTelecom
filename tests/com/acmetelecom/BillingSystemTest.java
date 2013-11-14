98ef000314264ea8087c9e319023b1f641eb8152
Yong Wen Chua <lawliet89@gmail.com>
2013-11-14 23:07:15 +0000

    Refactor BillingSystemTest to have better structure

--- tests/com/acmetelecom/BillingSystemTest.java
+++ tests/com/acmetelecom/BillingSystemTest.java
@@ -7,6 +7,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 import static org.junit.Assert.assertEquals;
@@ -15,42 +18,12 @@ import static org.junit.Assert.assertEquals;
 public class BillingSystemTest {
     BillingSystem billingSystem;
     String name;
-    ArrayList<Call> calls;
+    CallBuilder calls;
     HashMap<String, BigDecimal> expectedCost;
     List<Bill> bills;
 
-    /** Helper Methods **/
-
-    @SuppressWarnings("deprecation")
-    private static long getTimestamp(int day, int hour, int minute) {
-        Date date = new Date();
-        date.setDate(day);
-        date.setMonth(3);
-        date.setYear(2013);
-        date.setHours(hour);
-        date.setMinutes(minute);
-        date.setSeconds(0);
-        return date.getTime();
-
-    }
-
-    private static Call makeCall(String caller, String callee, int startDay, int startHour, int startMinute,
-                                 int endDay, int endHour, int endMinute) {
-        CallStart start = new CallStart(caller, callee, getTimestamp(startDay, startHour, startMinute));
-        CallEnd end = new CallEnd(caller, callee, getTimestamp(endDay, endHour, endMinute));
-        return new Call(start, end);
-    }
-
-    /*
-        Each set of data in the test data consists of a 3-tuple:
-            - 1: Name
-            - 2: List of calls
-            - 3: Hash map of expected cost
-
-     */
-
     @Parameterized.Parameters
-    public static Collection<Object[]> testData() {
+    public static Collection<Object[]> testData() throws ParseException {
 
         List<PeakPeriod> singlePeakPeriods = new ArrayList<PeakPeriod>();
         singlePeakPeriods.add(new PeakPeriod(7, 19));
@@ -65,56 +38,66 @@ public class BillingSystemTest {
         threePeakPeriods.add(new PeakPeriod(20, 21));
 
         // Off-peak test data
-        ArrayList<Call> offPeakCalls = new ArrayList<Call>();
-
-        offPeakCalls.add(makeCall("1", "2", 1, 20, 00, 1, 20, 05));
-        offPeakCalls.add(makeCall("2", "1", 1, 20, 10, 1, 20, 15));
-        offPeakCalls.add(makeCall("3", "2", 1, 20, 20, 1, 20, 25));
-
-        HashMap<String, BigDecimal> offPeakExpectedCost = new HashMap<String, BigDecimal>();
-        offPeakExpectedCost.put("1", new BigDecimal(60));
-        offPeakExpectedCost.put("2", new BigDecimal(0));
-        offPeakExpectedCost.put("3", new BigDecimal(0));
-
-        // Peak test data
-        ArrayList<Call> peakCalls = new ArrayList<Call>();
-
-        peakCalls.add(makeCall("1", "2", 1, 16, 00, 1, 16, 5));
-        peakCalls.add(makeCall("2", "1", 1, 16, 10, 1, 16, 15));
-        peakCalls.add(makeCall("3", "2", 1, 16, 20, 1, 16, 25));
-
-        HashMap<String, BigDecimal> peakExpectedCost = new HashMap<String, BigDecimal>();
-        peakExpectedCost.put("1", new BigDecimal(150));
-        peakExpectedCost.put("2", new BigDecimal(90));
-        peakExpectedCost.put("3", new BigDecimal(240));
+//        ArrayList<Call> offPeakCalls = new ArrayList<Call>();
+//
+//        offPeakCalls.add(makeCall("1", "2", 1, 20, 00, 1, 20, 05));
+//        offPeakCalls.add(makeCall("2", "1", 1, 20, 10, 1, 20, 15));
+//        offPeakCalls.add(makeCall("3", "2", 1, 20, 20, 1, 20, 25));
+//
+//        HashMap<String, BigDecimal> offPeakExpectedCost = new HashMap<String, BigDecimal>();
+//        offPeakExpectedCost.put("1", new BigDecimal(60));
+//        offPeakExpectedCost.put("2", new BigDecimal(0));
+//        offPeakExpectedCost.put("3", new BigDecimal(0));
+//
+//        // Peak test data
+//        ArrayList<Call> peakCalls = new ArrayList<Call>();
+//
+//        peakCalls.add(makeCall("1", "2", 1, 16, 00, 1, 16, 5));
+//        peakCalls.add(makeCall("2", "1", 1, 16, 10, 1, 16, 15));
+//        peakCalls.add(makeCall("3", "2", 1, 16, 20, 1, 16, 25));
+//
+//        HashMap<String, BigDecimal> peakExpectedCost = new HashMap<String, BigDecimal>();
+//        peakExpectedCost.put("1", new BigDecimal(150));
+//        peakExpectedCost.put("2", new BigDecimal(90));
+//        peakExpectedCost.put("3", new BigDecimal(240));
 
         // Hybrid test data
-        ArrayList<Call> hybridCalls = new ArrayList<Call>();
+        CallBuilder hybridCalls = new CallBuilder();
 
-        hybridCalls.add(makeCall("1", "2", 1, 6, 00, 1, 20, 00));
-        hybridCalls.add(makeCall("2", "1", 1, 18, 58,1,  19, 03));
-        hybridCalls.add(makeCall("3", "2", 1, 6, 58, 1, 7, 03));
+        hybridCalls.setCaller("1").setCallee("2")
+                .setStartDate("1-11-2013 06:00:00").setEndDate("1-11-2013 20:00:00")
+                .setExpectedPeakTime(43200).add();
+
+        hybridCalls.setCaller("2").setCallee("1")
+                .setStartDate("1-11-2013 18:58:00").setEndDate("1-11-2013 19:03:00")
+                .setExpectedPeakTime(120).add();
+
+        hybridCalls.setCaller("3").setCallee("2")
+                .setStartDate("1-11-2013 06:58:00").setEndDate("1-11-2013 07:03:00")
+                .setExpectedPeakTime(180).add();
 
         HashMap<String, BigDecimal> hybridExpectedCost = new HashMap<String, BigDecimal>();
-        hybridExpectedCost.put("1", new BigDecimal(23160));
+        hybridExpectedCost.put("1", new BigDecimal(23040));
         hybridExpectedCost.put("2", new BigDecimal(90));
         hybridExpectedCost.put("3", new BigDecimal(156));
 
         return Arrays.asList(new Object[][] {
-                { "Off-peak", offPeakCalls, offPeakExpectedCost, singlePeakPeriods },
-                { "Peak", peakCalls, peakExpectedCost, singlePeakPeriods },
+//                { "Off-peak", offPeakCalls, offPeakExpectedCost, singlePeakPeriods },
+//                { "Peak", peakCalls, peakExpectedCost, singlePeakPeriods },
                 { "Hybrid", hybridCalls, hybridExpectedCost, singlePeakPeriods }
         });
     }
 
-    public BillingSystemTest(String name, ArrayList<Call> calls, HashMap<String, BigDecimal> expectedCost, List<PeakPeriod> peakPeriods){
-        billingSystem = new BillingSystem(MockCustomerDatabase.getInstance(), CentralTariffDatabase.getInstance(), peakPeriods);
+    public BillingSystemTest(String name, CallBuilder calls, HashMap<String, BigDecimal> expectedCost,
+                             List<PeakPeriod> peakPeriods){
+        billingSystem = new BillingSystem(MockCustomerDatabase.getInstance(),
+                CentralTariffDatabase.getInstance(), peakPeriods);
         this.name = name;
         this.calls = calls;
         this.expectedCost = expectedCost;
 
         // "Make" the calls
-        for (Call call : calls) {
+        for (Call call : calls.calls()) {
             String caller = call.caller();
             String callee = call.callee();
 
@@ -127,7 +110,7 @@ public class BillingSystemTest {
     }
 
     @Test
-    public void testCalls() throws NoSuchFieldException {
+    public void testTotalBillCost() throws NoSuchFieldException {
         for (Bill bill : bills) {
             Customer customer = bill.getCustomer();
             BigDecimal expected = expectedCost.get(customer.getPhoneNumber());
@@ -141,11 +124,11 @@ public class BillingSystemTest {
     public void testLineItems() throws NoSuchFieldException {
         for (Bill bill : bills) {
             String phoneNumber = bill.getCustomer().getPhoneNumber();
-            // Extract list of expected calls
-            ArrayList<Call> expectedCalls = new ArrayList<Call>();
-            for (Call call : calls) {
-                if (call.caller().equals(phoneNumber)) {
-                    expectedCalls.add(call);
+            // Extract list of expected calls and their peak durations
+            ArrayList<Map.Entry<Call, Integer>> expectedCalls = new ArrayList<Map.Entry<Call, Integer>>();
+            for (Map.Entry<Call, Integer> pair : calls.callsExpectedPeakTimes().entrySet()) {
+                if (pair.getKey().caller().equals(phoneNumber)) {
+                    expectedCalls.add(pair);
                 }
             }
 
@@ -155,13 +138,92 @@ public class BillingSystemTest {
                     expectedCalls.size(), actualCalls.size());
 
             for (int i = 0; i < expectedCalls.size(); i++) {
-                Call expected = expectedCalls.get(i);
+                Call expected = expectedCalls.get(i).getKey();
                 BillingSystem.LineItem actual = actualCalls.get(i);
 
-                assertEquals(expected.callee(), actual.callee());
-                assertEquals(expected.caller(), actual.caller());
+                assertEquals(String.format("[%s] Customer %s callee", name, phoneNumber),
+                        expected.callee(), actual.callee());
+                assertEquals(String.format("[%s] Customer %s caller", name, phoneNumber),
+                        expected.caller(), actual.caller());
+                assertEquals(String.format("[%s] Customer %s peak-time", name, phoneNumber),
+                        expectedCalls.get(i).getValue().intValue(), actual.getPeakCallTime());
             }
         }
+    }
 
+    /**
+     * Helper nested classes
+     */
+
+    /**
+     * Call Builder class to make it easier to make calls
+     */
+    private static class CallBuilder {
+        public static DateFormat formatter = new SimpleDateFormat("d-M-y H:m:s");
+
+        // "Holder" members
+        private String caller, callee, startDate, endDate;
+        private int expectedPeakTime;
+
+        private LinkedHashMap<Call, Integer> calls;
+
+        public CallBuilder() {
+            caller = "";
+            callee = "";
+            startDate = "";
+            endDate = "";
+
+            calls = new LinkedHashMap<Call, Integer>();
+        }
+
+        private long getTimestamp(String date) throws ParseException {
+            return formatter.parse(date).getTime();
+        }
+
+        public CallBuilder setCaller(String caller) {
+            this.caller = caller;
+            return this;
+        }
+
+        public CallBuilder setCallee(String callee) {
+            this.callee = callee;
+            return this;
+        }
+
+        public CallBuilder setStartDate(String startDate) {
+            this.startDate = startDate;
+            return this;
+        }
+
+        public CallBuilder setEndDate(String endDate) {
+            this.endDate = endDate;
+            return this;
+        }
+
+        public CallBuilder setExpectedPeakTime(int expectedPeakTime) {
+            this.expectedPeakTime = expectedPeakTime;
+            return this;
+        }
+
+        public CallBuilder add() throws ParseException {
+            CallStart start = new CallStart(caller, callee, getTimestamp(startDate));
+            CallEnd end = new CallEnd(caller, callee, getTimestamp(endDate));
+            Call call = new Call(start, end);
+
+            calls.put(call, expectedPeakTime);
+            return this;
+        }
+
+        public Collection<Integer> expectedPeakTimes() {
+            return calls.values();
+        }
+
+        public Collection<Call> calls() {
+            return calls.keySet();
+        }
+
+        public LinkedHashMap<Call, Integer> callsExpectedPeakTimes() {
+            return calls;
+        }
     }
 }
