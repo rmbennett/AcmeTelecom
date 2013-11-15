0cdbf671706a8275beb9cc724f1494e8aeeffe25
Richard Bennett <rmb209@ic.ac.uk>
2013-11-15 15:28:21 +0000

    Move LineItem class from BillingSystem to Bill

--- src/com/acmetelecom/Bill.java
+++ src/com/acmetelecom/Bill.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 public class Bill {
     protected Customer customer;
-    protected List<BillingSystem.LineItem> calls;
+    protected List<LineItem> calls;
     protected BigDecimal totalBill;
 
-    public Bill(Customer customer, List<BillingSystem.LineItem> calls, BigDecimal totalBill) {
+    public Bill(Customer customer, List<LineItem> calls, BigDecimal totalBill) {
         this.customer = customer;
         this.calls = calls;
         this.totalBill = totalBill;
@@ -20,7 +20,7 @@ public class Bill {
         return customer;
     }
 
-    public List<BillingSystem.LineItem> getCalls() {
+    public List<LineItem> getCalls() {
         return calls;
     }
 
@@ -31,10 +31,46 @@ public class Bill {
     public void send() {
         Printer printer = HtmlPrinter.getInstance();
         printer.printHeading(customer.getFullName(), customer.getPhoneNumber(), customer.getPricePlan());
-        for (BillingSystem.LineItem call : calls) {
+        for (LineItem call : calls) {
             printer.printItem(call.date(), call.callee(), call.durationMinutes(), MoneyFormatter.penceToPounds(call.cost()));
         }
         printer.printTotal(MoneyFormatter.penceToPounds(totalBill));
     }
 
+    static class LineItem {
+        private Call call;
+        private BigDecimal callCost;
+
+        private int peakCallTime;
+
+        public LineItem(Call call, BigDecimal callCost, int peakCallTime) {
+            this.call = call;
+            this.callCost = callCost;
+            this.peakCallTime = peakCallTime;
+        }
+
+        public String date() {
+            return call.date();
+        }
+
+        public String caller() {
+            return call.caller();
+        }
+
+        public String callee() {
+            return call.callee();
+        }
+
+        public String durationMinutes() {
+            return "" + call.durationSeconds() / 60 + ":" + String.format("%02d", call.durationSeconds() % 60);
+        }
+
+        public BigDecimal cost() {
+            return callCost;
+        }
+
+        public int getPeakCallTime() {
+            return peakCallTime;
+        }
+    }
 }
